74c2a79c84e82e5c5a470fdb77b3c7b0b58d82cf
hussein mashat <huhumashat46@gmail.com>
2023-11-12 22:35:23 +0300

    6.3 العامل arguments

--- index.php
+++ index.php
@@ -1,41 +1,13 @@
 <?php
-    // funciton
-    function is_odd($number){
-        echo $number % 2 ? "{$number} is odd" : "{$number} is even";
-    }
-    function calculate($n1,$n2, $calcu) 
-    {
-        switch($calcu)
-        {
-        case "+": 
-            $compute = $n1 + $n2; 
-            break;
-        case "-":
-            $compute = $n1 - $n2; 
-            break;
-        case "*":
-            $compute = $n1 * $n2; 
-            break;
-        case "/":
-            $compute = $n1 / $n2; 
-            break;
-        case "^":
-            $compute = $n1 ** $n2; 
-            break;
-        default:
-            $compute = $n1 + $n2; 
-            break;
-        }
-        return $compute;
+    // Arguments 
+    function sum (...$numbers){
+        $acc = 0;
+        foreach ($numbers as $number) $acc += $number;
+        return  $acc;
     }
 
-    $a=5;
-    $b= "^";
-    $c= 2;
+    $arr = [1,2,3,4,5,6,7,8,9];
 
-    echo "{$a} {$b} {$c} = " . calculate($a,$c,$b) . "<br>";
-
-    is_odd(23);
-
-
+    echo "If we collect all this numbers ( ". implode(" + ", $arr) . " ) The answer it will be = " .  sum(...$arr);
+   
 ?>
