48d1d4c7d4e5725af3512d465d98a5259f6c96ba
hussein mashat <huhumashat46@gmail.com>
2023-11-12 14:35:58 +0300

    3.1 الجملة الشرطية if...else

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
    $num1 = 1;
-   $num2 = 2;
+   $num2 = 0;
 
 
-   echo "{$num1} == {$num2} = " . (integer)($num1 == $num2) . "<br>";
-   echo "{$num1} === {$num2} = " . (integer)($num1 === $num2) . "<br>";
-   echo "{$num1} !== {$num2} = " . (integer)($num1 !== $num2) . "<br>";
-   echo "{$num1} <= {$num2} = " . (integer)($num1 <= $num2) . "<br>";
-   echo "{$num1} < {$num2} = " . (integer)($num1 < $num2) . "<br>";
-   echo "{$num1} >= {$num2} = " .(integer)($num1 >= $num2) . "<br>";
-   echo "{$num1} > {$num2} = " . (integer)($num1 > $num2) . "<br>";
-   echo "{$num1} <=> {$num2} = " . (integer)($num1 <=> $num2) . "<br>";
+   if ($num1 > $num2) {
+    echo "a is bigger than b";
+   } else if ($num1 == $num2) {
+    echo "a is equal to b";
+   }else{
+    echo "a is not bigger than b";
+   }
 ?>
