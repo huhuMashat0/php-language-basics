12febaeaa75ef538fe200bffa3549cfb5c04c089
hussein mashat <huhumashat46@gmail.com>
2023-11-12 22:08:18 +0300

    6.1 الدوال في لغة PHP

--- index.php
+++ index.php
@@ -1,24 +1,17 @@
 <?php
-    // count
-    echo "count() => " . count(["Hello", "World"]) . "<br>";
+    // funciton
+    function print1To10(){
+        $i = 1;
+        while ($i <= 10) {
+            echo $i++ ."<br>";
+        }
 
-    // is _array
-    echo "is_array() => " . (integer) is_array([1,2,3]). " >> it's array<br>";
-    echo "is_array() => " . (integer) is_array('Hello'). " >>it is not array<br>";
+        function start(){
+            echo "You caaaan doooooo ittttt";
+        }
+    }
 
-    // explode
-    $string = "php javascript ruby swift kotlin";
-    $arr = explode(" ", $string);
-    echo "explode() => ";
-    print_r($arr);
 
-    //implode
-    echo "<br>implode() => ". implode(" - ", $arr) . "<br>";
-
-    // array_merge
-    $arr1 = ['red', 'green', 'blue'];
-    $arr2 = ['cyan','magenta','yellow'];
-    $result = array_merge($arr1, $arr2);
-    echo "array_merge() => ";
-    print_r($result);
+    print1To10();
+    start();
 ?>
