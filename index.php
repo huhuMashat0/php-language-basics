300e725ede5e15b425cc518f55dc3713a56732e9
hussein mashat <huhumashat46@gmail.com>
2023-11-12 12:27:37 +0300

    1.5 الثوابت Constants

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
 <?php
-    // echo gettype("Hello");
-    // echo gettype(28);
-
-    // *** casting ***
-
-    // to string
-    $var =  10;
-    echo gettype((string) $var);
+    // variable
+    $name = "Hsob";
+    echo $name;
     echo "<br>";
 
-    // to integer
-    echo (integer) '5Hello';
-    echo "<br>";
-
-    // to boolean
-    echo (boolean) "Hi"; 
-    echo "<br>";
+    // constants
+    const max_value = 100;
+    echo max_value;
 ?>
