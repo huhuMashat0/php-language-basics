0e3e1e67654e0fd163a1edb9420af8891246bb40
hussein mashat <huhumashat46@gmail.com>
2023-11-13 23:18:02 +0300

    7.1 الأصناف Classes

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <?php
-    // strtings
+    // classes
 
-    $str = "The Quick Brown Fox Jumps Over the lazy dog";
+    class Vehicle{
+        // Attributes
+        public $model;
+        public $year;
 
-    echo "The sentence used is ( " . $str . " )<br><br>";
 
-    echo "strlen(str) => ". strlen($str) . "<br>";
-
-    echo "strtolower(str) => ". strtolower($str) . "<br>";
-    
-    echo "strtoupper(str) => ". strtoupper($str) . "<br>";
-
-    echo "strpos(str, 'Q') => ". strpos($str,'Q') . "<br>";
-
-    echo "str_replace('dog','monkey', str) => ". str_replace('dog','monkey', $str) . "<br>"; 
-
-    echo "substr(str,5,10) => ". substr($str,4,5) . "<br>"; 
+        // Methods
+        public function start(){
+            echo "Engine started!";
+        }
+        public function stop(){
+            echo "Engine stopped!";
+        }
+    }
 ?>
