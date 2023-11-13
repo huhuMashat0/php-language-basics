25f432af7947094dee7a49fda2a15119025c7607
hussein mashat <huhumashat46@gmail.com>
2023-11-13 11:55:00 +0300

    6.6 الدوال السهمية Arrow Functions

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
-    // closure function
+    // arrow functions (php => 7.4)
 
     $arr = [1,2,3,4,5];
+    $num = 4;
 
-    $func = function($value){
-        return $value *2;
-    };
+    // $func = function($value) use ($num){
+    //     return $value * $num;
+    // };
+
+    $func = fn($value) => $value * $num;
+
 
     print_r(
         array_map($func, $arr)
