52cc61e64196c28daa7c58895ac429f6553009ca
hussein mashat <huhumashat46@gmail.com>
2023-11-15 02:24:37 +0300

    8.2 المعامل الفارغ الآمن Null Safe Operator

--- index.php
+++ index.php
@@ -1,15 +1,41 @@
 <?php
-    //try...catch
-    function division ($a,$b){
-        if ($b == 0) throw new Exception("Can't divide on 0. <br>");
-        return $a / $b;
+    //Null safe operator (PHP >= v.8)
+    class Address {
+        public $country;
+        public function __construct($country) {
+            $this->country = $country;
+        }
     }
 
-    try{
-        echo division(4,0) . "<br>";
-    } catch (Exception $e) {
-        echo $e->getMessage();
-    }finally{
-        echo "Finally";
+    class User{
+        public $address;
+        public function __construct($country) {
+            $this->address = new Address($country);
+        }
     }
+
+    class Session{
+        public $user;
+        public function __construct($address) {
+            // $this->user = new User($address);
+        }
+    }
+
+    $session = new Session('India');
+    // way 1
+    // $address = $session->user ? $session->user->address : null;
+    // way 2
+    // $country = null;
+    // if($session !== null){
+    //     $user = $session -> user;
+    //     if($user !== null){
+    //         $address = $user -> address;
+    //         if($address !== null){
+    //             $country = $address -> country;
+    //         }
+    //     }
+    // }
+    // way 3 - Null safe operator (PHP >= v.8)
+    $country = $session?->user?->address?->country;
+
 ?>
