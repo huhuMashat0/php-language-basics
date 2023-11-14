2aadd9f5a22d4b6599178ac6323b72ba4e8887b1
hussein mashat <huhumashat46@gmail.com>
2023-11-14 12:26:39 +0300

    7.10 الدوال السحرية Magic Methods

--- index.php
+++ index.php
@@ -1,16 +1,33 @@
 <?php
-    //Traits
-    trait MyTrait {
-        public function hello() {
-            echo "Hello World!";
+    //Magic Methods
+
+    class Example {
+        public function  __construct() {
+            echo 'Constructor initialised <br>';
+        }
+
+        public function __call($method, $args) {
+            echo "Unkown method ({$method}) - (";
+            print_r($args);
+            echo ")<br>";
+        }
+
+        public function __get($name){
+            echo "Unknown method ({$name})<br>";
+        }
+
+        public function __set($name, $value) {
+            echo "({$name}) is ({$value}) <br>";
+        }
+
+        public function __destruct() {
+            echo "Object gets destroyed automatically <br>";
         }
     }
 
-    class Example {
-        use MyTrait;
-    }
-
     $example = new Example();
-    $example->hello();
+    $example->hello('greet',2023);
+    $example->test;
+    $example->great = "Hello World";
 
 ?>
