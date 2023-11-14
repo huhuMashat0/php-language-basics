a4eb36bcb06eb1c916238406486f235d2f074e50
hussein mashat <huhumashat46@gmail.com>
2023-11-14 12:00:49 +0300

    7.7 الأصناف المجردة Abstract Classes

--- index.php
+++ index.php
@@ -1,86 +1,35 @@
 <?php
-    // classes
-    class Vehicle{
-        // Attributes
-        protected $model;
-        protected $year;
-        public static $CONSTANT = 'constant value';
+    // Abstract Classes
+    abstract class Shape{
+        abstract function area();
+    }
 
-        // constructor
-        public function __construct($model, $year){
-            $this->model = $model;
-            $this->year = $year;
+    class Square extends Shape{
+        private $length;
+        public function __construct($length){
+            $this->length = $length;
         }
 
-        public function getModel(){
-            return $this->model;
-        }
-
-        public function setModel($model){
-            $this->model = $model;
-        }
-
-        public function getYear(){
-            return $this->year;
-        }
-
-        public function setYear($year){
-            if(is_int($year)) $this->year = $year;
-            else throw new Exception("Invalid year!");
-        }
-
-
-        // Methods
-        public function start(){
-            echo "{$this->model} engine started!";
-        }
-        public function stop(){
-            echo "{$this->model} engine stopped!";
-        }
-        public function printInfo(){
-            echo "Name = " . $this->model ."<br>";
-            echo "Year = " . $this->year ."<br>";
-        }
-
-        public static function showConstant(){
-            // echo $this->model; <= this is will not work
-            echo self::$CONSTANT;
+        public function area(){
+            return $this->length * $this->length;
         }
     }
 
-    // Inheritance
-    class Car extends Vehicle{
-        private $color;
-        public function __construct($model,$year, $color){
-            parent::__construct($model,$year);
-            $this->color = $color;
+    class Circle extends Shape{
+        private $radius;
+        public function __construct($radius){
+            $this->radius = $radius;
         }
-        // polymorphism | Overriding
-        public function printInfo(){
-            parent::printInfo();
-            echo "Color = " . $this->color ."<br>";
+
+        public function area(){
+            return pi() * ($this->radius * $this->radius);
         }
     }
 
+    $sqrt = new Square(2);
+    echo "Area square = " . $sqrt->area() . "<br>";
 
-    // Object
-    $vehicle = new Vehicle("BMW",2023);
-    $vehicle->start();
-
-    // Encapsulation
-    $vehicle->setModel("Honda");
-    echo "<br>" . $vehicle->getModel();
-
-    // $vehicle->setYear("Honda");
-    $vehicle->setYear(2022);
-    echo " - " . $vehicle->getYear() . "<br>";
-
-    $car = new Car("Toyota",2024, 'orange');
-    $car -> printInfo();
-
-    // from static
-    // echo "<br>" . Vehicle::$CONSTANT;
-    $vehicle->showConstant();
-    
+    $circle = new Circle(2);
+    echo "Area circle = " . $circle->area() ."<br>";
 
 ?>
