159b74d634e9b9f739cee66d56cd484c56b4533a
hussein mashat <huhumashat46@gmail.com>
2023-11-14 12:12:52 +0300

    7.9 السمات Traits

--- index.php
+++ index.php
@@ -1,30 +1,16 @@
 <?php
-    // Interfaces
-    interface ShapeInterface {
-        public function perimeter();
-    }
-    // Abstract Classes
-    abstract class Shape{
-        abstract function area();
-    }
-
-    class Square extends Shape{
-        private $length;
-        public function __construct($length){
-            $this->length = $length;
-        }
-
-        public function area(){
-            return $this->length * $this->length;
-        }
-
-        public function periment(){
-            return $this->length * 4;
+    //Traits
+    trait MyTrait {
+        public function hello() {
+            echo "Hello World!";
         }
     }
 
-    $sqrt = new Square(2);
-    echo "Area square = " . $sqrt->area() . "<br>";
-    echo "Periment square = " . $sqrt->periment() . "<br>";
+    class Example {
+        use MyTrait;
+    }
+
+    $example = new Example();
+    $example->hello();
 
 ?>
